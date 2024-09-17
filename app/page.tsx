4b1c6f62163b1b60020fae6e0c3cdc572337b199
Chhaya More <aniketmore.personal@gmail.com>
2024-09-17 10:18:35 +0530

    Your commit message

--- app/page.tsx
+++ app/page.tsx
@@ -75,7 +75,7 @@ export default function LoginForm() {
               href="https://github.com/aniketmore-pixel"
               target="_blank"
               rel="noopener noreferrer"
-              className="text-xl px-8 py-4 bg-transparent hover:bg-white hover:text-purple-900 transition-colors duration-300"
+              className="inline-block text-xl px-8 py-4 bg-transparent border-2 border-transparent hover:border-white hover:bg-white hover:text-purple-900 transition-colors duration-300 rounded-full"
             >
               Give me a star on Github ⭐️
             </a>
