b9f42280a559a1ceac891ac5bc7256965c6a619f
Aniket More <aniketmore.personal@gmail.com>
2025-09-18 23:28:33 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -65,7 +65,7 @@ export default function LoginForm() {
           </form>
 
           <Button
-  className="text-lg px-6 py-3 border-2 border-white rounded-full bg-transparent text-purple-900 hover:bg-white hover:text-purple-900 transition duration-300"
+  className="text-lg px-6 py-3 border-2 border-white rounded-full bg-transparent text-white hover:bg-white hover:text-purple-900 transition duration-300"
   variant="outline"
 >
   <a
@@ -77,6 +77,7 @@ export default function LoginForm() {
     ⭐️ Star me on GitHub
   </a>
 </Button>
+
 
         </div>
 
